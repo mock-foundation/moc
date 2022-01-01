8951f4e7d8a920cfacf2181fc10002f62a2beaf4
ggoraa <ggoraa1029@gmail.com>
2022-01-01 04:49:05 +0200

    Added some notification keys

---
+++ Moc/NotificationKeys.swift
@@ -0,0 +1,56 @@
+//
+//  NotificationKeys.swift
+//  Moc
+//
+//  Created by Егор Яковенко on 01.01.2022.
+//
+
+import Foundation
+
+/// A ton of custom keys for representing notifications from TDLib.
+extension NSNotification.Name {
+    // MARK: - Authorization states
+    static var authorizationStateWaitTdlibParameters: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitTdlibParameters")
+    }
+
+    static var authorizationStateWaitEncryptionKey: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitEncryptionKey")
+    }
+
+    static var authorizationStateWaitPhoneNumber: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitPhoneNumber")
+    }
+
+    static var authorizationStateWaitCode: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitCode")
+    }
+
+    static var authorizationStateWaitRegistration: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitRegistration")
+    }
+
+    static var authorizationStateWaitPassword: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitPassword")
+    }
+
+    static var authorizationStateReady: Notification.Name {
+        return .init(rawValue: "AuthorizationStateReady")
+    }
+
+    static var authorizationStateWaitOtherDeviceConfirmation: Notification.Name {
+        return .init(rawValue: "AuthorizationStateWaitOtherDeviceConfirmation")
+    }
+
+    static var authorizationStateLoggingOut: Notification.Name {
+        return .init(rawValue: "AuthorizationStateLoggingOut")
+    }
+
+    static var authorizationStateClosing: Notification.Name {
+        return .init(rawValue: "AuthorizationStateClosing")
+    }
+
+    static var authorizationStateClosed: Notification.Name {
+        return .init(rawValue: "AuthorizationStateClosed")
+    }
+}
