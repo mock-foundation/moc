2ef3dc6667e90ae681cfa760c29dfc6e370f725d
ggoraa <ggoraa1029@gmail.com>
2022-01-01 04:49:43 +0200

    add injection helper functions to MocApp

--- Moc/MocApp+Injection.swift
+++ Moc/MocApp+Injection.swift
@@ -1,6 +1,6 @@
 //
 //  MocApp.swift
-//  Shared
+//  Moc
 //
 //  Created by Егор Яковенко on 24.12.2021.
 //
@@ -9,20 +9,39 @@ import SwiftUI
 import TDLibKit
 import Resolver
 
+extension Resolver {
+    public static func registerViewModels() {
+        register { MainViewModel() }
+    }
+
+    public static func registerTd() {
+        register { TdApi(client: TdClientImpl()) }
+        register { TdService() }
+        NSLog("td registration successful")
+    }
+}
+
 @main
 struct MocApp: App {
-	init() {
-		Resolver.register {
-			MainViewModel()
-		}
-		Resolver.register {
-			TdApi(client: TdClientImpl())
-		}
-	}
-	
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    init() {
+        Resolver.registerViewModels()
+        Resolver.registerTd()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView().onAppear {
+                InitService().initService()
+            }
+        }
+    }
+}
+
+// kostil'
+private class InitService {
+    @Injected private var tdService: TdService
+    
+    public func initService() {
+        tdService.initHandler()
+    }
 }
