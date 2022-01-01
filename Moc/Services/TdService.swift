7602533ed91ac09771a5890a0f6112c73021c1d6
ggoraa <ggoraa1029@gmail.com>
2022-01-01 04:46:28 +0200

    implemented TdService()

--- Moc/Services/TdService.swift
+++ Moc/Services/TdService.swift
@@ -9,73 +9,101 @@ import TDLibKit
 import Resolver
 import Foundation
 
+/// Runs everything needed for TDLib to run, and contains a global update handler.
+/// Usually the update handler just posts a notification to the default NotificationCenter about an update, but
+/// sometimes it can handle the update **and** post a notification about it.
 class TdService {
-	@Injected private var tdApi: TdApi
-	init() {
-		tdApi.client.run {
-			do {
-				let update = try self.tdApi.decoder.decode(Update.self, from: $0)
-				NSLog("CaughtUpdate: \(update)")
-				switch update {
-					case .updateAuthorizationState(let state):
-						switch state.authorizationState {
-							case .authorizationStateWaitTdlibParameters:
-								Task(priority: .medium) {
-									NSLog("something")
-									let _ = try! await self.tdApi.setTdlibParameters(parameters: TdlibParameters(
-										apiHash: Bundle.main.infoDictionary?["TdApiHash"] as! String,
-										apiId: Bundle.main.infoDictionary?["TdApiId"] as! Int,
-										applicationVersion: "0.1",
-										databaseDirectory: "",
-										deviceModel: "MacBook Pro X lol",
-										enableStorageOptimizer: true,
-										filesDirectory: "",
-										ignoreFileNames: false,
-										systemLanguageCode: "en-US",
-										systemVersion: "Monterey 12.0",
-										useChatInfoDatabase: true,
-										useFileDatabase: true,
-										useMessageDatabase: true,
-										useSecretChats: false,
-										useTestDc: true
-									))
-								}
-								break
-							case .authorizationStateWaitEncryptionKey(_):
-								Task(priority: .medium) {
-									try! await self.tdApi.checkDatabaseEncryptionKey(encryptionKey: nil)
-								}
-								break
-							case .authorizationStateWaitPhoneNumber:
-								NotificationCenter.default.post(name: Notification.Name("AuthorizationPhoneNumberRequired"), object: nil)
-								NSLog("phone number")
-								break
-							case .authorizationStateWaitCode(_):
-								NotificationCenter.default.post(name: Notification.Name("AuthorizationCodeRequired"), object: nil)
-								NSLog("codeRequired")
-								break
-							case .authorizationStateWaitRegistration(_):
-								NotificationCenter.default.post(name: Notification.Name("AuthorizationWaitRegistration"), object: nil)
-								NSLog("registration")
-								break
-							case .authorizationStateWaitPassword(_):
-								NotificationCenter.default.post(name: Notification.Name("AuthorizationPasswordRequired"), object: nil)
-								NSLog("password")
-								break
-							case .authorizationStateReady:
-								NotificationCenter.default.post(name: Notification.Name("AuthorizationSuccessful"), object: nil)
-								print("auth successful")
-								break
-							default:
-								break
-						}
-						break
-					default:
-						break
-				}
-			} catch {
-//							print("Error in update handler \(error.localizedDescription)")
-			}
-		}
-	}
+    @Injected private var tdApi: TdApi
+
+    public func initHandler() {
+        tdApi.client.run {
+            do {
+                let update = try self.tdApi.decoder.decode(Update.self, from: $0)
+                switch update {
+                        // MARK: - Authorization state
+                    case .updateAuthorizationState(let state):
+                        switch state.authorizationState {
+                            case .authorizationStateWaitTdlibParameters:
+                                self.post(notification: .authorizationStateWaitTdlibParameters)
+                                Task(priority: .medium) {
+                                    let _ = try! await self.tdApi.setTdlibParameters(parameters: TdlibParameters(
+                                        apiHash: Bundle.main.infoDictionary?["TdApiHash"] as! String,
+                                        apiId: Bundle.main.infoDictionary?["TdApiId"] as! Int,
+                                        applicationVersion: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String,
+                                        databaseDirectory: "",
+                                        deviceModel: self.getMacModel() ?? "Unknown",
+                                        enableStorageOptimizer: true,
+                                        filesDirectory: "",
+                                        ignoreFileNames: false,
+                                        systemLanguageCode: "en-US",
+                                        systemVersion: self.getOSVersionString(),
+                                        useChatInfoDatabase: true,
+                                        useFileDatabase: true,
+                                        useMessageDatabase: true,
+                                        useSecretChats: false,
+                                        useTestDc: true
+                                    ))
+                                }
+                            case .authorizationStateWaitEncryptionKey(_):
+                                self.post(notification: .authorizationStateWaitEncryptionKey)
+                                Task(priority: .medium) {
+                                    try! await self.tdApi.checkDatabaseEncryptionKey(encryptionKey: nil)
+                                }
+                            case .authorizationStateWaitPhoneNumber:
+                                self.post(notification: .authorizationStateWaitPhoneNumber)
+                            case .authorizationStateWaitCode(let info):
+                                self.post(notification: .authorizationStateWaitCode, withObject: info)
+                            case .authorizationStateWaitRegistration(let info):
+                                self.post(notification: .authorizationStateWaitRegistration, withObject: info)
+                            case .authorizationStateWaitPassword(let info):
+                                self.post(notification: .authorizationStateWaitPassword, withObject: info)
+                            case .authorizationStateReady:
+                                self.post(notification: .authorizationStateReady)
+                            case .authorizationStateWaitOtherDeviceConfirmation(let info):
+                                self.post(notification: .authorizationStateWaitOtherDeviceConfirmation, withObject: info)
+                            case .authorizationStateLoggingOut:
+                                self.post(notification: .authorizationStateLoggingOut)
+                            case .authorizationStateClosing:
+                                self.post(notification: .authorizationStateClosing)
+                            case .authorizationStateClosed:
+                                self.post(notification: .authorizationStateClosed)
+                        }
+                    default:
+                        NSLog("Unhandled TDLib update \(update)")
+                }
+            } catch {
+                NSLog("Error in TDLib update handler \(error.localizedDescription)")
+            }
+        }
+
+    }
+
+    private func post(notification: NSNotification.Name) {
+        NotificationCenter.default.post(name: notification, object: nil)
+    }
+
+    private func post(notification: NSNotification.Name, withObject obj: Any?) {
+        NotificationCenter.default.post(name: notification, object: obj)
+    }
+
+    // Thanks to https://www.reddit.com/r/swift/comments/gwf9fa/how_do_i_find_the_model_of_the_mac_in_swift/
+    func getMacModel() -> String? {
+        let service = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("IOPlatformExpertDevice"))
+        var modelIdentifier: String?
+
+        if let modelData = IORegistryEntryCreateCFProperty(service, "model" as CFString, kCFAllocatorDefault, 0).takeRetainedValue() as? Data {
+            if let modelIdentifierCString = String(data: modelData, encoding: .utf8)?.cString(using: .utf8) {
+                modelIdentifier = String(cString: modelIdentifierCString)
+            }
+        }
+
+        IOObjectRelease(service)
+        return modelIdentifier
+    }
+
+    func getOSVersionString() -> String {
+        let info = ProcessInfo().operatingSystemVersionString
+        NSLog(info)
+        return info
+    }
 }
