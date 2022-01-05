f560732e31e9db1cb27b2b3e2be32583c0f644a5
ggoraa <ggoraa1029@gmail.com>
2022-01-05 22:45:45 +0200

    feat: Updated getMacModel in MocApp. Now it returns a marketing name instead of a model identifier

--- Moc/MocApp+Injection.swift
+++ Moc/MocApp+Injection.swift
@@ -94,7 +94,8 @@ extension Resolver {
     }
 
     // Thanks to https://www.reddit.com/r/swift/comments/gwf9fa/how_do_i_find_the_model_of_the_mac_in_swift/
-    static func getMacModel() -> String? {
+    static func getMacModel() -> String {
+        // Get device identifier
         let service = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("IOPlatformExpertDevice"))
         var modelIdentifier: String?
 
@@ -105,7 +106,14 @@ extension Resolver {
         }
 
         IOObjectRelease(service)
-        return modelIdentifier
+
+        // And then find a corresponding marketing name using the identifier
+        let serverInfoBundle = Bundle(path: "/System/Library/PrivateFrameworks/ServerInformation.framework/")
+        let sysInfoFile = serverInfoBundle?.url(forResource: "SIMachineAttributes", withExtension: "plist")
+        let plist = NSDictionary(contentsOfFile: sysInfoFile!.path)
+
+        // TODO: Replace force-unwraps with something more safe
+        return ((plist![modelIdentifier!] as! NSDictionary)["_LOCALIZABLE_"] as! NSDictionary)["marketingModel"] as! String
     }
 
     static func getOSVersionString() -> String {
