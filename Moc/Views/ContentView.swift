ba31c3c78664de8d28c14e5a78f82b5482915c4f
ggoraa <ggoraa1029@gmail.com>
2022-01-06 03:10:14 +0200

    feat: Ui improvements and start logic for implementing chat lists

--- Moc/Views/ContentView.swift
+++ Moc/Views/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 import TDLibKit
 import Resolver
 
+extension Chat: Identifiable {
+
+}
+
 struct ContentView: View {
 	@State private var selectedFolder: Int = 0
 	@State private var selectedChat: Int? = -1
@@ -35,26 +39,26 @@ struct ContentView: View {
                             List(mainViewModel.chatList, selection: $selectedChat) { chat in
 								NavigationLink(destination: {
 									GeometryReader { proxy in
-										ChatView()
+                                        ChatView(chat: chat)
 											.frame(width: proxy.size.width, height: proxy.size.height)
 											.navigationTitle("")
 											.toolbar {
 												ToolbarItem(placement: .navigation) {
-                                                    Image("MockChatLogo")
+                                                    Image("MockChatPhoto")
                                                         .resizable()
                                                         .frame(width: 32, height: 32)
                                                         .clipShape(Circle())
 												}
 												ToolbarItem(placement: .navigation) {
 													VStack(alignment: .leading) {
-                                                        Text(chat.name)
+                                                        Text(chat.title)
 															.font(.headline)
 														Text("Some users were here lol")
 															.font(.subheadline)
 													}.onTapGesture(count: 2) {
-                                                        Task {
-                                                            try! await tdApi.sendMessage(chatId: 736211268, inputMessageContent: .inputMessageText(InputMessageText(clearDraft: true, disableWebPagePreview: true, text: FormattedText(entities: [], text: "Это сообщение было отправлено из Moc!"))), messageThreadId: nil, options: nil, replyMarkup: nil, replyToMessageId: nil)
-                                                        }
+//                                                        Task {
+//                                                            try! await tdApi.sendMessage(chatId: 736211268, inputMessageContent: .inputMessageText(InputMessageText(clearDraft: true, disableWebPagePreview: true, text: FormattedText(entities: [], text: "Это сообщение было отправлено из Moc!"))), messageThreadId: nil, options: nil, replyMarkup: nil, replyToMessageId: nil)
+//                                                        }
 														selectedChat = 2
 													}
 												}
@@ -82,6 +86,11 @@ struct ContentView: View {
 										.frame(height: 56)
 								}
 							}
+                            .swipeActions {
+                                Button(role: .destructive) { NSLog("Pressed Delete button") } label: {
+                                    Label("Delete chat", systemImage: "trash")
+                                }
+                            }
 							.toolbar {
 								ToolbarItem(placement: .status) {
 									Button(action: {
@@ -101,22 +110,13 @@ struct ContentView: View {
 			LoginView()
 				.frame(width: 300, height: 400)
 		}
-        .onReceive(NotificationCenter.default.publisher(for: .updateChatLastMessage)) { data in
+        .onReceive(NotificationCenter.default.publisher(for: .updateNewChat)) { data in
             NSLog("Received chat position update")
-            Task {
-                let chatIds = try! await tdApi.getChats(chatList: .chatListMain, limit: 30).chatIds
-                var chatList: [Chat] = []
-                var parsedChatList: [ChatItem] = []
-                for id in chatIds {
-                    chatList.append(try! await tdApi.getChat(chatId: id))
-                }
-                parsedChatList = chatList.map { chat in
-                    return ChatItem(id: chat.id, name: chat.title, messagePreview: "Message preview", sender: "Message sender", showSender: false, type: .group, chatIcon: Image("MockChatLogo"), isPinned: false, time: Date(timeIntervalSinceNow: 0), seen: false)
-                }
-                mainViewModel.update(chats: parsedChatList)
-            }
+            let chat = (data.object as! UpdateNewChat).chat
+            mainViewModel.chatList.append(chat)
         }
-        .onReceive(NotificationCenter.default.publisher(for: .authorizationStateWaitPhoneNumber)){ data in
+        .onReceive(NotificationCenter.default.publisher(for: .authorizationStateWaitPhoneNumber)) { data in
+            NSLog("Phone number update lol")
 			showingLoginScreen = true
 		}
 	}
