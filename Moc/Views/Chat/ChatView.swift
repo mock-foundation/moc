72430117dea2d0c06ac07a99a7254a981ebebf36
ggoraa <ggoraa1029@gmail.com>
2022-01-09 17:23:50 +0200

    chore: fix warnings in ChatView

--- Moc/Views/Chat/ChatView.swift
+++ Moc/Views/Chat/ChatView.swift
@@ -54,7 +54,7 @@ struct ChatView: View {
     }
 
     // MARK: - Additional inspector stuff
-    private func InspectorButton(action: @escaping () -> Void, imageName: String, text: String) -> some View {
+    private func inspectorButton(action: @escaping () -> Void, imageName: String, text: String) -> some View {
         Button(action: action) {
             VStack(spacing: 8) {
                 Image(systemName: imageName)
@@ -67,7 +67,7 @@ struct ChatView: View {
         .buttonStyle(.borderless)
     }
 
-    private func UserRow(name: String, status: UserStatus, photo: Image? = nil) -> some View {
+    private func userRow(name: String, status: UserStatus, photo: Image? = nil) -> some View {
         HStack {
             if photo != nil {
                 photo!
@@ -112,19 +112,19 @@ struct ChatView: View {
 
                 // Quick actions
                 HStack(spacing: 24) {
-                    InspectorButton(
+                    inspectorButton(
                         action: {  },
                         imageName: "person.crop.circle.badge.plus",
                         text: "Add"
                     )
                     Divider()
-                    InspectorButton(
+                    inspectorButton(
                         action: {  },
                         imageName: "bell.slash",
                         text: "Mute"
                     )
                     Divider()
-                    InspectorButton(
+                    inspectorButton(
                         action: {  },
                         imageName: "arrow.turn.up.right",
                         text: "Leave"
@@ -148,7 +148,7 @@ struct ChatView: View {
                     switch selectedInspectorTab {
                         case .users:
                             ForEach(0..<10) { index in
-                                UserRow(name: "User \(index)", status: .userStatusRecently, photo: Image("MockChatPhoto"))
+                                userRow(name: "User \(index)", status: .userStatusRecently, photo: Image("MockChatPhoto"))
                                     .padding(.horizontal, 8)
                                     .frame(minWidth: 0, idealWidth: nil)
                             }
@@ -213,7 +213,23 @@ struct ChatView: View {
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
-        ChatView(chat: Chat(actionBar: .none, canBeDeletedForAllUsers: true, canBeDeletedOnlyForSelf: true, canBeReported: true, clientData: "", defaultDisableNotification: true, draftMessage: nil, hasProtectedContent: false, hasScheduledMessages: false, id: 10294934 /* i just banged my head against the keyboard, so this number is completely random */, isBlocked: false, isMarkedAsUnread: false, lastMessage: nil, lastReadInboxMessageId: 102044379 /* the same */, lastReadOutboxMessageId: 39439379573 /* again */, messageSenderId: nil, messageTtl: 0, notificationSettings: ChatNotificationSettings(disableMentionNotifications: true, disablePinnedMessageNotifications: true, muteFor: 10, showPreview: false, sound: "", useDefaultDisableMentionNotifications: true, useDefaultDisablePinnedMessageNotifications: true, useDefaultMuteFor: true, useDefaultShowPreview: true, useDefaultSound: true), pendingJoinRequests: nil, permissions: ChatPermissions(canAddWebPagePreviews: true, canChangeInfo: true, canInviteUsers: true, canPinMessages: true, canSendMediaMessages: true, canSendMessages: true, canSendOtherMessages: true, canSendPolls: true), photo: nil, positions: [], replyMarkupMessageId: 1023948920349 /* my head hurts */, themeName: "", title: "Curry Club - Ninjas from the reeds", type: .chatTypeBasicGroup(.init(basicGroupId: 102343920 /* i really should use a proper random number generator instead of using my head as a random number generator */)), unreadCount: 0, unreadMentionCount: 0, videoChat: VideoChat(defaultParticipantId: nil, groupCallId: 0, hasParticipants: false)))
+        ChatView(chat: Chat(
+            actionBar: .none,
+            canBeDeletedForAllUsers: true,
+            canBeDeletedOnlyForSelf: true,
+            canBeReported: true,
+            clientData: "",
+            defaultDisableNotification: true,
+            draftMessage: nil,
+            hasProtectedContent: false,
+            hasScheduledMessages: false,
+            id: 10294934 /* i just banged my head against the keyboard, so this number is completely random */,
+            isBlocked: false,
+            isMarkedAsUnread: false,
+            lastMessage: nil,
+            lastReadInboxMessageId: 102044379 /* the same */,
+            lastReadOutboxMessageId: 39439379573 /* again */,
+            messageSenderId: nil, messageTtl: 0, notificationSettings: ChatNotificationSettings(disableMentionNotifications: true, disablePinnedMessageNotifications: true, muteFor: 10, showPreview: false, sound: "", useDefaultDisableMentionNotifications: true, useDefaultDisablePinnedMessageNotifications: true, useDefaultMuteFor: true, useDefaultShowPreview: true, useDefaultSound: true), pendingJoinRequests: nil, permissions: ChatPermissions(canAddWebPagePreviews: true, canChangeInfo: true, canInviteUsers: true, canPinMessages: true, canSendMediaMessages: true, canSendMessages: true, canSendOtherMessages: true, canSendPolls: true), photo: nil, positions: [], replyMarkupMessageId: 1023948920349 /* my head hurts */, themeName: "", title: "Curry Club - Ninjas from the reeds", type: .chatTypeBasicGroup(.init(basicGroupId: 102343920 /* i really should use a proper random number generator instead of using my head as a random number generator */)), unreadCount: 0, unreadMentionCount: 0, videoChat: VideoChat(defaultParticipantId: nil, groupCallId: 0, hasParticipants: false)))
             .frame(width: 800, height: 600)
     }
 }
