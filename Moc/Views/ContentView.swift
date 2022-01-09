7c198a468501f69cfb25a32aefbb3df99cf7804c
GGorAA <ggoraa1029@gmail.com>
2022-01-10 01:04:44 +0200

    View router (#16)
    * feat: base version of a custom view router
    
    * other: small updates to updateNewChat receiver in ContentView
    
    * refactor: renamed some colors
    
    * feat: a working ViewRouter
    
    * feat: moved UI from using NavigationLink to my ViewRouter
    Benefits: significantly better memory management, more fine-grained control
    Drawbacks: i need to make UI by myself
    
    * xcode: stuff
    
    * feat: finished UI part of the view router

--- Moc/Views/ContentView.swift
+++ Moc/Views/ContentView.swift
@@ -14,11 +14,12 @@ extension Chat: Identifiable { }
 struct ContentView: View {
     @State private var selectedFolder: Int = 0
     @State private var selectedChat: Int? = 0
+    @State private var showingLoginScreen = false
+
     @StateObject private var mainViewModel = MainViewModel()
+    @StateObject private var viewRouter = ViewRouter()
 
     @Injected private var tdApi: TdApi
-
-    @State private var showingLoginScreen = false
 
     var body: some View {
         NavigationView {
@@ -34,18 +35,25 @@ struct ContentView: View {
                         SearchField()
                             .padding([.leading, .bottom, .trailing], 10.0)
                         List(mainViewModel.chatList) { chat in
-                            NavigationLink(tag: Int(chat.id), selection: $selectedChat) {
-                                ChatView(chat: chat)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            } label: {
-                                ChatItemView(chat: chat)
-                                    .frame(height: 56)
-                            }
-                            .swipeActions {
-                                Button(role: .destructive) { NSLog("Pressed Delete button") } label: {
-                                    Label("Delete chat", systemImage: "trash")
+                            ChatItemView(chat: chat)
+                                .frame(height: 52)
+                                .swipeActions(edge: .trailing) {
+                                    Button(role: .destructive) { NSLog("Pressed Delete button") } label: {
+                                        Label("Delete chat", systemImage: "trash")
+                                    }
                                 }
-                            }
+                                .onTapGesture {
+                                    viewRouter.openedChat = chat
+                                    viewRouter.currentView = .chat
+                                }
+                                .padding(6)
+                                .background(
+                                    (viewRouter.currentView == .chat
+                                     && viewRouter.openedChat! == chat)
+                                    ? Color.blue.opacity(0.8)
+                                    : nil
+                                )
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
                         }
                     }.toolbar {
                         ToolbarItem(placement: .status) {
@@ -57,6 +65,18 @@ struct ContentView: View {
                 }
             }
             .listStyle(.sidebar)
+
+            switch viewRouter.currentView {
+                case .selectChat:
+                    VStack {
+                        Text("Select chat")
+                    }
+                case .chat:
+                    VStack {
+                        ChatView(chat: viewRouter.openedChat!)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+            }
         }
         .sheet(isPresented: $showingLoginScreen) {
             LoginView()
@@ -82,12 +102,12 @@ struct ContentView: View {
                 } else {
                     return true
                 }
-//
-//                if $0.lastMessage?.date ?? 1 > $1.lastMessage?.date ?? 0 {
-//                    return true
-//                } else {
-//                    return false
-//                }
+                //
+                //                if $0.lastMessage?.date ?? 1 > $1.lastMessage?.date ?? 0 {
+                //                    return true
+                //                } else {
+                //                    return false
+                //                }
             })
 
         }
---
+++ Moc/Views/ViewRouter.swift
@@ -0,0 +1,21 @@
+//
+//  ViewRouter.swift
+//  Moc
+//
+//  Created by Егор Яковенко on 06.01.2022.
+//
+
+import SwiftUI
+import TDLibKit
+
+/// A simple view router for managing open chat state. Is **not** designed for more.
+final class ViewRouter: ObservableObject {
+
+    public enum Views: CaseIterable {
+        case selectChat
+        case chat
+    }
+
+    @Published var openedChat: Chat?
+    @Published var currentView: Views = .selectChat
+}
