8d856970dd6be76af86c89867d596a459c8539b6
ggoraa <ggoraa1029@gmail.com>
2022-01-12 16:31:43 +0200

    feat: created ImageUtils module
    This module will contain all code that is related to image handling, also with some convenient type extensions.

---
+++ ImageUtils/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ImageUtils",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "ImageUtils",
+            targets: ["ImageUtils"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ImageUtils",
+            dependencies: []),
+        .testTarget(
+            name: "ImageUtilsTests",
+            dependencies: ["ImageUtils"])
+    ]
+)
