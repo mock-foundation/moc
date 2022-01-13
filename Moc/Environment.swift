95a1f3bcaae03235c2df8e7d3be855a9e1a4747e
ggoraa <ggoraa1029@gmail.com>
2022-01-13 20:11:54 +0200

    feat: added environment file

---
+++ Moc/Environment.swift
@@ -0,0 +1,16 @@
+//
+//  Environment.swift
+//  Moc
+//
+//  Created by Егор Яковенко on 13.01.2022.
+//
+
+extension MocApp {
+    public static var apiId: Int {
+        return 09873 // i should stop using my head banging on the keyboard as a random number generator
+    }
+
+    public static var apiHash: String {
+        return "ayy" // really
+    }
+}
