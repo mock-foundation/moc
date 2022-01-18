f1581af4a4d8bb45f90a01304bc4eeb31058f8ee
ggoraa <ggoraa1029@gmail.com>
2022-01-18 12:27:18 +0200

    feat: created Backend module for encapsulating tdlib logic

---
+++ Backend/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Backend",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "Backend",
+            targets: ["Backend"])
+    ],
+    dependencies: [
+        .package(
+            name: "TDLibKit",
+            url: "https://github.com/Swiftgram/TDLibKit.git",
+            .revisionItem("e78aef926cdd92323755bcb72309aa5afed1f02a")
+        )
+    ],
+    targets: [.target(
+            name: "Backend",
+            dependencies: [])
+    ]
+)
---
+++ Backend/README.md
@@ -0,0 +1,5 @@
+# Backend
+
+This package contains all code that talks with the server, and some
+mock ones for testing and previews. It basically abstracts away all
+TDLib stuff from the app, so any API changes are easily handled.
