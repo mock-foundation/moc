d953a145012f80362cf16f723d4b32fe598a63d1
ggoraa <ggoraa1029@gmail.com>
2022-01-21 22:56:08 +0200

    refactor: preferences refactor

---
+++ Moc/Views/LanguagePrefView.swift
@@ -0,0 +1,20 @@
+//
+//  LanguagePrefView.swift
+//  Moc
+//
+//  Created by Егор Яковенко on 12.01.2022.
+//
+
+import SwiftUI
+
+struct LanguagePrefView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LanguagePrefView_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguagePrefView()
+    }
+}
---
+++ Moc/Views/NotificationsAndSoundsPrefView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsAndSoundsPrefView.swift
+//  Moc
+//
+//  Created by Егор Яковенко on 12.01.2022.
+//
+
+import SwiftUI
+
+struct NotificationsAndSoundsPrefView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationsAndSoundsPrefView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsAndSoundsPrefView()
+    }
+}
