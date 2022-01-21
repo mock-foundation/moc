46f9f0d1bb0ecb641ac6538932eed589b09ed59c
ggoraa <ggoraa1029@gmail.com>
2022-01-21 23:00:32 +0200

    refactor: one more preferences refactor

--- Moc/MocApp+Injection.swift
+++ Moc/MocApp+Injection.swift
@@ -86,49 +86,7 @@ struct MocApp: App {
         }
 
         Settings {
-            TabView {
-                GeneralPrefView()
-                    .tabItem {
-                        Label("General", systemImage: "gear")
-                    }
-                NotificationsAndSoundsPrefView()
-                    .tabItem {
-                        Label("Notifications and Sounds", systemImage: "bell")
-                    }
-                PrivacyView()
-                    .tabItem {
-                        Label("Privacy", systemImage: "lock")
-                    }
-                DataAndStoragePrefView()
-                    .tabItem {
-                        Label("Data and Storage", systemImage: "externaldrive")
-                    }
-                DevicesPrefView()
-                    .tabItem {
-                        Label("Devices", systemImage: "laptopcomputer.and.iphone")
-                    }
-                AppearancePrefView()
-                    .tabItem {
-                        Label("Appearance", systemImage: "paintbrush")
-                    }
-                LanguagePrefView()
-                    .tabItem {
-                        Label("Language", systemImage: "globe")
-                    }
-                StickersPrefView()
-                    .tabItem {
-                        Label("Stickers", systemImage: "rectangle.3.group.bubble.left")
-                    }
-                FoldersPrefView()
-                    .tabItem {
-                        Label("Folders", systemImage: "folder")
-                    }
-                AccountsPrefView()
-                    .tabItem {
-                        Label("Accounts", systemImage: "person.circle")
-                    }
-            }
-            .frame(width: 800)
+            PreferencesContent()
         }
     }
 }
