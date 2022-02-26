3c27941c4447303b266e5e1f7234596725a26420
GGorAA <ggoraa1029@gmail.com>
2022-02-27 00:31:06 +0200

    feat: added Logging module

---
+++ Logging/Sources/Logging/Logger.swift
@@ -0,0 +1,63 @@
+import Foundation
+import os
+
+/// Logging implementation. Uses NSLog under the hood.
+public struct Logger {
+    private let logger: os.Logger
+
+    public init(label: String, category: String) {
+        self.logger = os.Logger(subsystem: label, category: category)
+    }
+
+    public func log(_ message: String?, level: LogLevel) {
+        switch level {
+        case .trace:
+            logger.trace("\(String(describing: message))")
+        case .debug:
+            logger.debug("\(String(describing: message))")
+        case .info:
+            logger.info("\(String(describing: message))")
+        case .notice:
+            logger.notice("\(String(describing: message))")
+        case .warning:
+            logger.warning("\(String(describing: message))")
+        case .error:
+            logger.error("\(String(describing: message))")
+        case .critical:
+            logger.critical("\(String(describing: message))")
+        }
+//        if let message = message {
+//            NSLog("\(label): \(level.rawValue): \(message)")
+//        } else {
+//            NSLog("\(label): \(level.rawValue)")
+//        }
+    }
+
+    public func trace(_ message: String?) {
+        log(message, level: .trace)
+    }
+
+    public func debug(_ message: String?) {
+        log(message, level: .debug)
+    }
+
+    public func info(_ message: String?) {
+        log(message, level: .info)
+    }
+
+    public func notice(_ message: String?) {
+        log(message, level: .notice)
+    }
+
+    public func warning(_ message: String?) {
+        log(message, level: .warning)
+    }
+
+    public func error(_ message: String?) {
+        log(message, level: .error)
+    }
+
+    public func critical(_ message: String?) {
+        log(message, level: .critical)
+    }
+}
