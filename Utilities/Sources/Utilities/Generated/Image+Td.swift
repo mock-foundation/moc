7fee7f070c5b4511bf58cf0d20987c0be75245b3
GGorAA <ggoraa1029@gmail.com>
2022-05-26 15:21:38 +0300

    feat: added a convenience initializer for initializing an image from a TD icon

---
+++ Utilities/Sources/Utilities/Generated/Image+Td.swift
@@ -0,0 +1,40 @@
+//
+//  Image+Td.swift
+//  
+//
+//  Created by Егор Яковенко on 26.05.2022.
+//
+
+import SwiftUI
+
+public extension Image {
+    // swiftlint:disable cyclomatic_complexity
+    init(tdIcon icon: String) {
+        switch icon {
+            case "All": self.init(systemName: "bubble.left.and.bubble.right")
+            case "Unread": self.init("systemName: exclamationmark.bubble")
+            case "Unmuted": self.init(systemName: "text.bubble")
+            case "Bots": self.init("bot")
+            case "Channels": self.init(systemName: "megaphone")
+            case "Groups": self.init(systemName: "person.2")
+            case "Private": self.init(systemName: "person")
+            case "Custom": self.init(systemName: "folder")
+            case "Setup": self.init(systemName: "list.bullet")
+            case "Cat": self.init(systemName: "pawprint")
+            case "Crown": self.init(systemName: "crown")
+            case "Favorite": self.init(systemName: "star")
+            case "Flower": self.init(systemName: "leaf")
+            case "Game": self.init(systemName: "gamecontroller")
+            case "Home": self.init(systemName: "house")
+            case "Love": self.init(systemName: "heart")
+            case "Mask": self.init(systemName: "theatermasks")
+            case "Party": self.init(systemName: "sparkles")
+            case "Sport": self.init(systemName: "sportscourt")
+            case "Study": self.init(systemName: "graduationcap")
+            case "Trade": self.init(systemName: "cart")
+            case "Travel": self.init(systemName: "airplane")
+            case "Work": self.init(systemName: "building.2")
+            default: self.init(systemName: "folder")
+        }
+    }
+}
