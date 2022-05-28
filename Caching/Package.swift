6bddc6b76937afef59a83cc259eab82659495b6c
GGorAA <ggoraa1029@gmail.com>
2022-05-29 02:19:49 +0300

    feat: added Realm dependency

--- Caching/Package.swift
+++ Caching/Package.swift
@@ -4,20 +4,25 @@ import PackageDescription
 
 let package = Package(
     name: "Caching",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
         .library(
             name: "Caching",
             targets: ["Caching"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-swift.git", from: "10.27.0")
+        .package(url: "https://github.com/realm/realm-swift.git", from: "10.27.0"),
+        .package(path: "../Utilities")
     ],
     targets: [
         .target(
             name: "Caching",
             dependencies: [
                 .product(name: "Realm", package: "realm-swift"),
-                .product(name: "RealmSwift", package: "realm-swift")
+                .product(name: "RealmSwift", package: "realm-swift"),
+                "Utilities"
             ])
     ]
 )
