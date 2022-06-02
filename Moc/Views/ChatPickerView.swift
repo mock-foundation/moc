20e071cc55aa7b6cb1a93f4e5587c9427a9af6f3
GGorAA <ggoraa1029@gmail.com>
2022-06-02 15:31:12 +0300

    feat: created ChatPickerView

---
+++ Moc/Views/ChatPickerView.swift
@@ -0,0 +1,25 @@
+//
+//  ChatPickerView.swift
+//  Moc
+//
+//  Created by Егор Яковенко on 02.06.2022.
+//
+
+import SwiftUI
+
+struct ChatPickerView: View {
+    var body: some View {
+        VStack {
+            SearchField()
+            List {
+                
+            }
+        }
+    }
+}
+
+struct ChatPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatPickerView()
+    }
+}
