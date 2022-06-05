2612fc5398eec1690541f71b6494821a2c0771c2
GGorAA <ggoraa1029@gmail.com>
2022-06-06 02:24:27 +0300

    feat: updated the CacheService API It now can modify and remove records by any row, not specifically an id And also made the code tidier

--- Caching/Sources/Caching/CacheService.swift
+++ Caching/Sources/Caching/CacheService.swift
@@ -55,22 +55,22 @@ private extension CacheService {
     }
 
     private func save<Record>(db: Database, record: Record) throws
-        where Record: FetchableRecord, Record: PersistableRecord {
+        where Record: FetchableRecord & PersistableRecord {
         try record.insert(db)
     }
 
     private func deleteAll<Record>(db: Database, records: Record.Type) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    where Record: FetchableRecord & PersistableRecord {
         try records.deleteAll(db)
     }
     
-    private func delete<Record>(from db: Database, record: Record.Type, at id: Int) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
-        try record.deleteOne(db, key: id)
+    private func delete<Record, Key>(from db: Database, record: Record.Type, at key: Key) throws
+    where Record: FetchableRecord & PersistableRecord, Key: DatabaseValueConvertible {
+        try record.deleteOne(db, key: key)
     }
     
     private func delete<Record>(from db: Database, record: Record) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    where Record: FetchableRecord & PersistableRecord {
         try record.delete(db)
     }
 
@@ -78,17 +78,17 @@ private extension CacheService {
         from db: Database,
         as record: Record.Type,
         ordered: [SQLOrderingTerm]) throws -> [Record]
-    where Record: FetchableRecord, Record: PersistableRecord {
+    where Record: FetchableRecord & PersistableRecord {
         try record.order(ordered).fetchAll(db)
     }
     
-    private func modify<Record>(
+    private func modify<Record, Key>(
         record: Record.Type,
-        at id: Int,
+        at key: Key,
         from db: Database,
         transform: (inout Record) -> Void) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
-        if var value = try record.filter(Column("id") == id).fetchOne(db) {
+    where Record: FetchableRecord & PersistableRecord, Key: DatabaseValueConvertible {
+        if var value = try record.fetchOne(db, key: key) {
             try value.updateChanges(db) {
                 transform(&$0)
             }
@@ -100,44 +100,49 @@ private extension CacheService {
 
 public extension CacheService {
     func save<Record>(record: Record) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    where Record: FetchableRecord & PersistableRecord {
         try dbQueue.write { db in
             try save(db: db, record: record)
         }
     }
 
     func deleteAll<Record>(records: Record.Type) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    where Record: FetchableRecord & PersistableRecord {
         try dbQueue.write { db in
             try deleteAll(db: db, records: records)
         }
     }
     
-    func delete<Record>(record: Record.Type, at id: Int) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    func delete<Record, Key>(record: Record.Type, at key: Key) throws
+    where Record: FetchableRecord & PersistableRecord, Key: DatabaseValueConvertible {
         try dbQueue.write { db in
-            try delete(from: db, record: record, at: id)
+            try delete(from: db, record: record, at: key)
         }
     }
     
     func delete<Record>(record: Record) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    where Record: FetchableRecord & PersistableRecord {
         try dbQueue.write { db in
             try delete(from: db, record: record)
         }
     }
 
-    func getRecords<Record>(as record: Record.Type, ordered: [SQLOrderingTerm] = []) throws -> [Record]
-    where Record: FetchableRecord, Record: PersistableRecord {
+    func getRecords<Record>(
+        as record: Record.Type,
+        ordered: [SQLOrderingTerm] = []) throws -> [Record]
+    where Record: FetchableRecord & PersistableRecord {
         try dbQueue.read { db in
             try getRecords(from: db, as: record, ordered: ordered)
         }
     }
     
-    func modify<Record>(record: Record.Type, at id: Int, transform: (inout Record) -> Void) throws
-    where Record: FetchableRecord, Record: PersistableRecord {
+    func modify<Record, Key>(
+        record: Record.Type,
+        at key: Key,
+        transform: (inout Record) -> Void) throws
+    where Record: FetchableRecord & PersistableRecord, Key: DatabaseValueConvertible {
         try dbQueue.write { db in
-            try modify(record: record, at: id, from: db, transform: transform)
+            try modify(record: record, at: key, from: db, transform: transform)
         }
     }
 }
