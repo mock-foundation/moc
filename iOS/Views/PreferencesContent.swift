7cab9f5d2fa456f07f79d4431cab67e6fd182f62
GGorAA <ggoraa1029@gmail.com>
2022-06-15 19:54:31 +0300

    feat: finished preferences for iPadOS well, partially, the UI is a little wacky but I have no idea how to fix it

--- iOS/Views/PreferencesContent.swift
+++ iOS/Views/PreferencesContent.swift
@@ -46,92 +46,89 @@ private enum PreferenceTab: CaseIterable {
 
 struct PreferencesContent: View {
     @State private var searchText = ""
-    @State private var selection: PreferenceTab? = nil
     @Environment(\.dismiss) private var dismiss
     
-    @ViewBuilder
-    private var sidebar: some View {
-        NavigationLink { GeneralPrefView() } label: {
-            Label("General", systemImage: "gear")
-        }
-        NavigationLink { NotificationsAndSoundsPrefView() } label: {
-            Label("Notifications and Sounds", systemImage: "bell")
-        }
-        NavigationLink { PrivacyPrefView() } label: {
-            Label("Privacy", systemImage: "lock")
-        }
-        NavigationLink { DataAndStoragePrefView() } label: {
-            Label("Data and Storage", systemImage: "externaldrive")
-        }
-        NavigationLink { DevicesPrefView() } label: {
-            Label("Devices", systemImage: "laptopcomputer.and.iphone")
-        }
-        NavigationLink { AppearancePrefView() } label: {
-            Label("Appearance", systemImage: "paintbrush")
-        }
-        NavigationLink { LanguagePrefView() } label: {
-            Label("Language", systemImage: "globe")
-        }
-        NavigationLink { StickersPrefView() } label: {
-            Label("Stickers", systemImage: "rectangle.3.group.bubble.left")
-        }
-        NavigationLink { FoldersPrefView() } label: {
-            Label("Folders", systemImage: "folder")
-        }
-        NavigationLink { AccountsPrefView() } label: {
-            Label("Accounts", systemImage: "person.circle")
-        }
-    }
-    
     var body: some View {
-        if #available(iOS 16, *) {
-            NavigationSplitView {
-                List(PreferenceTab.allCases, id: \.self, selection: $selection) { preference in
-                    NavigationLink {
-                        switch preference {
-                            case .general:
-                                GeneralPrefView()
-                            case .notificationsAndSounds:
-                                NotificationsAndSoundsPrefView()
-                            case .privacy:
-                                PrivacyPrefView()
-                            case .dataAndStorage:
-                                DataAndStoragePrefView()
-                            case .devices:
-                                DevicesPrefView()
-                            case .appearance:
-                                AppearancePrefView()
-                            case .language:
-                                LanguagePrefView()
-                            case .stickers:
-                                StickersPrefView()
-                            case .folders:
-                                FoldersPrefView()
+//        if #available(iOS 16, *) {
+//            NavigationSplitView {
+//                List(PreferenceTab.allCases, id: \.self) { preference in
+//                    NavigationLink {
+//                        switch preference {
+//                            case .general:
+//                                GeneralPrefView()
+//                            case .notificationsAndSounds:
+//                                NotificationsAndSoundsPrefView()
+//                            case .privacy:
+//                                PrivacyPrefView()
+//                            case .dataAndStorage:
+//                                DataAndStoragePrefView()
+//                            case .devices:
+//                                DevicesPrefView()
+//                            case .appearance:
+//                                AppearancePrefView()
+//                            case .language:
+//                                LanguagePrefView()
+//                            case .stickers:
+//                                StickersPrefView()
+//                            case .folders:
+//                                FoldersPrefView()
+//                        }
+//                    } label: {
+//                        preference.label
+//                    }
+//                }
+//                .toolbar {
+//                    ToolbarItem(placement: .navigationBarLeading) {
+//                        Button("Close") {
+//                            dismiss()
+//                        }
+//                    }
+//                }
+//            } detail: {
+//
+//            }
+//        } else {
+            NavigationView {
+                List(PreferenceTab.allCases, id: \.self) { preference in
+                    Section {
+                        NavigationLink {
+                            switch preference {
+                                case .general:
+                                    GeneralPrefView()
+                                case .notificationsAndSounds:
+                                    NotificationsAndSoundsPrefView()
+                                case .privacy:
+                                    PrivacyPrefView()
+                                case .dataAndStorage:
+                                    DataAndStoragePrefView()
+                                case .devices:
+                                    DevicesPrefView()
+                                case .appearance:
+                                    AppearancePrefView()
+                                case .language:
+                                    LanguagePrefView()
+                                case .stickers:
+                                    StickersPrefView()
+                                case .folders:
+                                    FoldersPrefView()
+                            }
+                        } label: {
+                            preference.label
                         }
-                    } label: {
-                        preference.label
                     }
-                }.toolbar {
+                }
+                .listStyle(.sidebar)
+                .navigationTitle("Settings")
+                .searchable(text: $searchText, placement: .sidebar)
+                .toolbar {
                     ToolbarItem(placement: .navigationBarLeading) {
                         Button("Close") {
                             dismiss()
                         }
                     }
                 }
-            } detail: {
-                
-            }
-        } else {
-            NavigationView {
-                List {
-                    sidebar
-                }
-                .listStyle(.sidebar)
-                .navigationTitle("Settings")
-                .searchable(text: $searchText, placement: .sidebar)
-                EmptyView()
             }.navigationViewStyle(.columns)
-        }
+//        }
     }
 }
 
