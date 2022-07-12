7838a7d34e4349e1c39489bdb6a118339126020f
GGorAA <ggoraa1029@gmail.com>
2022-07-12 15:06:10 +0300

    feat: changed the animation duration and curve

--- Moc/Views/Chat/ChatView.swift
+++ Moc/Views/Chat/ChatView.swift
@@ -43,7 +43,7 @@ struct ChatView: View {
                 .onReceive(SystemUtils.ncPublisher(for: .scrollToMessage)) { notification in
                     let id = notification.object as! Int64
                     viewModel.highlightMessage(at: id)
-                    withAnimation(.timingCurve(0, 0.99, 0.31, 1, duration: 1.5)) {
+                    withAnimation(.fastStartSlowStop) {
                         proxy.scrollTo(id, anchor: .center)
                     }
                 }
