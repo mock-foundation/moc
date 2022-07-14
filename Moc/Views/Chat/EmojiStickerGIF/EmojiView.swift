67bea1d38c86b9f903929cafce3a555181443135
dmikots <57399131+dmikots@users.noreply.github.com>
2022-07-14 21:10:42 +0300

    Add scroll bounds to start

--- Moc/Views/Chat/EmojiStickerGIF/EmojiView.swift
+++ Moc/Views/Chat/EmojiStickerGIF/EmojiView.swift
@@ -7,41 +7,54 @@
 
 import SwiftUI
 import Resolver
+import Networking
+
 struct EmojiView: View {
     
     @ObservedObject private  var viewModel: EmojiViewModel
+    @Namespace private var topId
+    @State private var navigateToNewCategory = false
     
     init(viewModel: EmojiViewModel) {
         self.viewModel = viewModel
     }
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center, spacing: 16) {
             //Category
             HStack(spacing: 12){
-                    ForEach(EmojiViewModel.EmojiCategory.allCases, id: \.id){ item in
-                        Text(item.rawValue)
-                            .onTapGesture {
-                                Task {
-                                    await viewModel.getEmojiFromCategory(emojiName:item.stringValue)
-                                }
+                ForEach(EmojiViewModel.EmojiCategory.allCases, id: \.id){ item in
+                    Text(item.rawValue)
+                        .onTapGesture {
+                            Task {
+                                await viewModel.getEmojiFromCategory(emojiName:item.stringValue)
                             }
-                    }
+                            navigateToNewCategory.toggle()
+                        }
+                }
             }
             TextField("Search", text: $viewModel.emojiSearch)
                 .padding(.horizontal)
-            ScrollView {
+            ScrollViewReader { scrollProxy in
+                ScrollView {
+                    Color.clear.frame(height: 1)
+                        .id(topId)
                     LazyVGrid(columns: Array(repeating: GridItem(), count: 6), spacing: 10) {
                         ForEach(viewModel.emoji , id: \.id) { item in
                             Text(item.emoji)
                                 .onTapGesture {
                                     
                                 }
-                                .id(0)
                         }
                     }
                 }
-            .frame(maxHeight: 150)
+                .frame(maxHeight: 150)
+            .onChange(of: navigateToNewCategory) {  _ in
+                    withAnimation {
+                        scrollProxy.scrollTo(topId)
+                    }
+                }
+            }
             .onAppear{
                 Task {
                     await viewModel.getFavoriteEmoji()
