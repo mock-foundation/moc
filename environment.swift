79d4b933c4922c0c384130f07dc0d2909bd79177
GGorAA <ggoraa1029@gmail.com>
2022-07-31 20:12:57 +0300

    feat: moved all functions to the EnvironmentScript struct, bc why are they global anyways

--- environment.swift
+++ environment.swift
@@ -20,6 +20,15 @@ let projectNameText = """
     |__/     |__/ \\______/  \\_______/
 """
 
+enum ScriptError: Error {
+    case parseError(String)
+    case runCommandFail(String)
+}
+
+enum UserChoice {
+    case yes
+    case no
+}
 
 struct EnvironmentScript: ParsableCommand {
     static let configuration = CommandConfiguration(
@@ -52,18 +61,18 @@ struct EnvironmentScript: ParsableCommand {
             ) {
                 if askForContinuation("Do you want to remove Homebrew packages related to Moc?") {
                     sectionStart("Homebrew output")
-                    try environment.run(command: "brew", with: ["remove", "swiftformat"])
-                    try environment.run(command: "brew", with: ["remove", "swiftlint"])
-                    try environment.run(command: "brew", with: ["remove", "gyb"])
-                    try environment.run(command: "brew", with: ["remove", "swiftgen"])
-                    try environment.run(command: "brew", with: ["remove", "sourcery"])
+                    try run(command: "brew", with: ["remove", "swiftformat"])
+                    try run(command: "brew", with: ["remove", "swiftlint"])
+                    try run(command: "brew", with: ["remove", "gyb"])
+                    try run(command: "brew", with: ["remove", "swiftgen"])
+                    try run(command: "brew", with: ["remove", "sourcery"])
                     sectionEnd()
                 }
                 
                 if askForContinuation("Do you want to clear generated code?") {
                     logInfo("Clearing generated code...")
-                    try environment.run(command: "rm", with: ["-rf", "Sources/Utilities/Generated"])
-                    try environment.run(command: "rm", with: ["-rf", "Moc/Generated"])
+                    try run(command: "rm", with: ["-rf", "Sources/Utilities/Generated"])
+                    try run(command: "rm", with: ["-rf", "Moc/Generated"])
                 }
                 
                 logOk("Finished!")
@@ -80,169 +89,171 @@ struct EnvironmentScript: ParsableCommand {
                 
                 logInfo("Running GYB...")
                 
+                try FileManager.default.createDirectory(
+                    atPath: "Utilities/Sources/Utilities/Generated",
+                    withIntermediateDirectories: true)
+                
+                
                 logError("Not implemented")
             } else {
                 logError("Not enought arguments supplied, please check if you did insert API ID and hash values.")
             }
         }
     }
+    
+    func envValue(for key: String) -> String? {
+        ProcessInfo.processInfo.environment[key]
+    }
+    
+    func log(_ message: String) {
+        print(">>> ".white.bold + message.bold)
+    }
+    
+    func logInfo(_ message: String) {
+        log(message.blue)
+    }
+    
+    func logOk(_ message: String) {
+        log(message.green)
+    }
+    
+    func logWarning(_ message: String) {
+        log(message.yellow)
+    }
+    
+    func logNotice(_ message: String) {
+        log(message.cyan)
+    }
+    
+    func logError(_ message: String) {
+        log(message.red)
+    }
+    
+    func sectionStart(_ message: String) {
+        print("\n" + "--- ".white.bold + message.white.bold + " ---".white.bold + "\n")
+    }
+    
+    func sectionEnd() {
+        print("\n------\n".white.bold)
+    }
+    
+    /// Installs a specified command if not available
+    /// - Parameters:
+    ///   - command: Command to be installed
+    ///   - brew: Name of the command in Homebrew
+    ///   - display: How this command's name is displayed in logs
+    func install(command: String, from brew: String, as display: String) throws {
+        if try runWithOutput(command: "which", with: [command]).contains(command) {
+            logOk("\(display) is installed")
+        } else {
+            logInfo("\(display) was not found, installing...")
+            sectionStart("Homebrew output")
+            try run(command: "brew", with: ["install", brew])
+            sectionEnd()
+        }
+    }
+    
+    /// Runs a supplied shell command.
+    /// - Parameters:
+    ///   - command: A command to run.
+    ///   - args: Args supplied to it
+    /// - Throws: Any error, like being unable to parse command's response or a run failure.
+    /// - Returns: Command's output
+    @discardableResult
+    func runWithOutput(command: String, with args: [String]) throws -> String {
+        let which = Process()
+        which.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+        which.arguments = [command] + args
+        
+        var pipe = Pipe()
+        which.standardOutput = pipe
+        
+        do {
+            try which.run()
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            if let output = String(data: data, encoding: .utf8) {
+                return output
+            } else {
+                throw ScriptError.parseError("Unable to parse '\(command)' command's output")
+            }
+        } catch {
+            throw ScriptError.runCommandFail("Unable to run command \(command)")
+        }
+    }
+    
+    /// Runs a supplied shell command.
+    /// - Parameters:
+    ///   - command: A command to run.
+    ///   - args: Args supplied to it
+    /// - Throws: Any error, like being unable to parse command's response or a run failure.
+    /// - Returns: Command's output
+    @discardableResult
+    func run(pwd: String? = nil, command: String, with args: [String]) throws -> Int32 {
+        let task = Process()
+        task.launchPath = "/usr/bin/env"
+        task.arguments = [command] + args
+        if let pwd {
+            task.currentDirectoryURL = URL(string: pwd)!
+        }
+        task.launch()
+        task.waitUntilExit()
+        return task.terminationStatus
+    }
+    
+    /// Runs `readLine()` that asks the user for `y` or `n` response.
+    /// - Parameter message: The message with the request.
+    /// - Parameter explicit: Whether to explicitly ask for `yes` or `no` response.
+    /// - Returns: User's choice
+    func askForContinuation(_ message: String, explicit: Bool = false, preferred: UserChoice = .yes) -> Bool {
+        // This mess is just a system for lighting the choices in different ways
+        print(
+            message.bold.underline + " "
+            + (explicit
+               ? "(" + (preferred == .yes
+                        ? "yes".green
+                        : "yes".lightRed)
+               + "/" + (preferred == .no
+                        ? "no".green
+                        : "no".lightRed) + ")"
+               : "(" + (preferred == .yes
+                        ? "Y".green
+                        : "Y".lightRed) + "/"
+               + (preferred == .no
+                  ? "n".green
+                  : "n".lightRed) + ")")
+            + " ", terminator: "")
+        
+        if let result = readLine() {
+            switch result.lowercased() {
+                case "y":
+                    if explicit {
+                        print()
+                        logNotice("Please write a full answer:")
+                        askForContinuation(message, explicit: explicit, preferred: preferred)
+                    } else {
+                        return true
+                    }
+                case "yes":
+                    return true
+                case "n":
+                    if explicit {
+                        logNotice("Please write a full answer:")
+                        askForContinuation(message, explicit: explicit, preferred: preferred)
+                    } else {
+                        return false
+                    }
+                case "no":
+                    return false
+                default:
+                    logNotice("Wrong response. Please try again:")
+                    askForContinuation(message, explicit: explicit, preferred: preferred)
+            }
+        } else {
+            logError("Unable to ask for continuation.")
+        }
+        
+        return false
+    }
 }
 
 EnvironmentScript.main()
-
-func log(_ message: String) {
-    print(">>> ".white.bold + message.bold)
-}
-
-func logInfo(_ message: String) {
-    log(message.blue)
-}
-
-func logOk(_ message: String) {
-    log(message.green)
-}
-
-func logWarning(_ message: String) {
-    log(message.yellow)
-}
-
-func logNotice(_ message: String) {
-    log(message.cyan)
-}
-
-func logError(_ message: String) {
-    log(message.red)
-}
-
-func sectionStart(_ message: String) {
-    print("\n" + "--- ".white.bold + message.white.bold + " ---".white.bold + "\n")
-}
-
-func sectionEnd() {
-    print("\n------\n".white.bold)
-}
-
-/// Installs a specified command if not available
-/// - Parameters:
-///   - command: Command to be installed
-///   - brew: Name of the command in Homebrew
-///   - display: How this command's name is displayed in logs
-func install(command: String, from brew: String, as display: String) throws {
-    if try runWithOutput(command: "which", with: [command]).contains(command) {
-        logOk("\(display) is installed")
-    } else {
-        logInfo("\(display) was not found, installing...")
-        sectionStart("Homebrew output")
-        try run(command: "brew", with: ["install", brew])
-        sectionEnd()
-    }
-}
-
-/// Runs a supplied shell command.
-/// - Parameters:
-///   - command: A command to run.
-///   - args: Args supplied to it
-/// - Throws: Any error, like being unable to parse command's response or a run failure.
-/// - Returns: Command's output
-@discardableResult
-func runWithOutput(command: String, with args: [String]) throws -> String {
-    let which = Process()
-    which.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-    which.arguments = [command] + args
-    
-    var pipe = Pipe()
-    which.standardOutput = pipe
-    
-    do {
-        try which.run()
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        if let output = String(data: data, encoding: .utf8) {
-            return output
-        } else {
-            throw ScriptError.parseError("Unable to parse '\(command)' command's output")
-        }
-    } catch {
-        throw ScriptError.runCommandFail("Unable to run command \(command)")
-    }
-}
-
-/// Runs a supplied shell command.
-/// - Parameters:
-///   - command: A command to run.
-///   - args: Args supplied to it
-/// - Throws: Any error, like being unable to parse command's response or a run failure.
-/// - Returns: Command's output
-@discardableResult
-func run(command: String, with args: [String]) throws -> Int32 {
-    let task = Process()
-    task.launchPath = "/usr/bin/env"
-    task.arguments = [command] + args
-    task.launch()
-    task.waitUntilExit()
-    return task.terminationStatus
-}
-
-/// Runs `readLine()` that asks the user for `y` or `n` response.
-/// - Parameter message: The message with the request.
-/// - Parameter explicit: Whether to explicitly ask for `yes` or `no` response.
-/// - Returns: User's choice
-func askForContinuation(_ message: String, explicit: Bool = false, preferred: UserChoice = .yes) -> Bool {
-    // This mess is just a system for lighting the choices in different ways
-    print(
-        message.bold.underline + " "
-        + (explicit
-           ? "(" + (preferred == .yes
-                ? "yes".green
-                : "yes".lightRed)
-           + "/" + (preferred == .no
-                ? "no".green
-                : "no".lightRed) + ")"
-           : "(" + (preferred == .yes
-                ? "Y".green
-                : "Y".lightRed) + "/"
-           + (preferred == .no
-                ? "n".green
-                : "n".lightRed) + ")")
-        + " ", terminator: "")
-    
-    if let result = readLine() {
-        switch result.lowercased() {
-            case "y":
-                if explicit {
-                    print()
-                    logNotice("Please write a full answer:")
-                    askForContinuation(message, explicit: explicit, preferred: preferred)
-                } else {
-                    return true
-                }
-            case "yes":
-                return true
-            case "n":
-                if explicit {
-                    logNotice("Please write a full answer:")
-                    askForContinuation(message, explicit: explicit, preferred: preferred)
-                } else {
-                    return false
-                }
-            case "no":
-                return false
-            default:
-                logNotice("Wrong response. Please try again:")
-                askForContinuation(message, explicit: explicit, preferred: preferred)
-        }
-    } else {
-        logError("Unable to ask for continuation.")
-    }
-
-    return false
-}
-
-enum ScriptError: Error {
-    case parseError(String)
-    case runCommandFail(String)
-}
-
-enum UserChoice: String {
-    case yes
-    case no
-}
