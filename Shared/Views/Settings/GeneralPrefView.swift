7ce694fffa77aa305170d8fadc2a30c257a28c88
GGorAA <ggoraa1029@gmail.com>
2022-08-11 00:26:10 +0300

    feat: started improving UI design of the chat shortcuts editor

--- Shared/Views/Settings/GeneralPrefView.swift
+++ Shared/Views/Settings/GeneralPrefView.swift
@@ -37,45 +37,51 @@ struct GeneralPrefView: View {
                     .font(.caption)
             }
             .sheet(isPresented: $isChatShortcutsSheetOpen) {
-                VStack {
-                    Button {
-                        isChatShortcutsSheetOpen = false
-                    } label: {
-                        Image(systemName: "xmark")
-                    }
-                    .buttonStyle(.plain)
-                    .hTrailing()
-                    .padding()
-                    List {
-                        ForEach(chatShortcuts, id: \.self) { chatId in
-                            HStack {
-                                CompactChatItemView(chatId: chatId)
-                            }
-                            .swipeActions(edge: .trailing) {
-                                Button(role: .destructive) {
-                                    chatShortcuts.removeAll(where: { $0 == chatId })
-                                } label: {
-                                    Label("Remove", systemImage: "trash")
+                HStack {
+                    VStack {
+                        Text("Chat shortcuts")
+                            .font(.largeTitle)
+                        Text("Save chats to app's menubar for easy access from any place in Moc.")
+                            .font(.title3)
+                    }.frame(width: 100)
+                    VStack {
+                        Button {
+                            isChatShortcutsSheetOpen = false
+                        } label: {
+                            Image(systemName: "xmark")
+                        }
+                        .buttonStyle(.plain)
+                        .hTrailing()
+                        List {
+                            ForEach(chatShortcuts, id: \.self) { chatId in
+                                HStack {
+                                    CompactChatItemView(chatId: chatId)
+                                }
+                                .swipeActions(edge: .trailing) {
+                                    Button(role: .destructive) {
+                                        chatShortcuts.removeAll(where: { $0 == chatId })
+                                    } label: {
+                                        Label("Remove", systemImage: "trash")
+                                    }
                                 }
                             }
                         }
-                    }
-                    #if os(macOS)
-                    .listStyle(.bordered(alternatesRowBackgrounds: true))
-                    #endif
-                    Button("Add") {
-                        isNewChatShortcutSheetOpen = true
-                    }
-                    .sheet(isPresented: $isNewChatShortcutSheetOpen) {
-                        VStack(spacing: 8) {
-                            Button {
-                                isNewChatShortcutSheetOpen = false
-                            } label: {
-                                Image(systemName: "xmark")
-                            }
-                            .buttonStyle(.plain)
-                            .hTrailing()
-                            Text(
+                        #if os(macOS)
+                        .listStyle(.bordered(alternatesRowBackgrounds: true))
+                        #endif
+                        Button("Add") {
+                            isNewChatShortcutSheetOpen = true
+                        }
+                        .sheet(isPresented: $isNewChatShortcutSheetOpen) {
+                            VStack(spacing: 8) {
+                                Button {
+                                    isNewChatShortcutSheetOpen = false
+                                } label: {
+                                    Image(systemName: "xmark")
+                                }
+                                .buttonStyle(.plain)
+                                .hTrailing()
+                                Text(
                             """
                             Chat picker is in development, will be done in Stage 3
                             Please insert the chat ID instead, you can find it \
@@ -85,18 +91,19 @@ struct GeneralPrefView: View {
                                 .lineLimit(nil)
                                 .multilineTextAlignment(.center)
                                 .fixedSize(horizontal: false, vertical: true)
-                            TextField("Chat ID", value: $chatId, formatter: NumberFormatter())
-                                .onSubmit {
-                                    if chatId != 0 {
-                                        chatShortcuts.append(chatId)
-                                        isNewChatShortcutSheetOpen = false
-                                        self.chatId = 0
+                                TextField("Chat ID", value: $chatId, formatter: NumberFormatter())
+                                    .onSubmit {
+                                        if chatId != 0 {
+                                            chatShortcuts.append(chatId)
+                                            isNewChatShortcutSheetOpen = false
+                                            self.chatId = 0
+                                        }
                                     }
-                                }
-                                .textFieldStyle(.roundedBorder)
+                                    .textFieldStyle(.roundedBorder)
+                            }
+                            .frame(maxWidth: 200, maxHeight: 300)
+                            .padding()
                         }
-                        .frame(maxWidth: 200, maxHeight: 300)
-                        .padding()
                     }
                 }
                 .frame(minWidth: 250, minHeight: 300)
