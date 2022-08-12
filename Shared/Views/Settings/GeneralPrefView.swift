efb9bc2bf021b1371ffd6cd3d981a270dfa24cc0
GGorAA <ggoraa1029@gmail.com>
2022-08-12 15:38:40 +0300

    feat: greatly improved the design of the general settings tab

--- Shared/Views/Settings/GeneralPrefView.swift
+++ Shared/Views/Settings/GeneralPrefView.swift
@@ -10,107 +10,106 @@ import Defaults
 import Utilities
 
 struct GeneralPrefView: View {
-    @State private var isChatShortcutsSheetOpen = false
     @State private var isNewChatShortcutSheetOpen = false
     @State private var chatId: Int64 = 0
 
     @Default(.chatShortcuts) private var chatShortcuts
         
     var body: some View {
-        Form {
-            Section {
-                Defaults.Toggle("Show developer info", key: .showDeveloperInfo)
-            } footer: {
-                Text("""
-                    When enabled, Moc will show additional data in the \
-                    chat inspector like the chat/user ID.
-                    """)
-                .font(.caption)
-                .fixedSize(horizontal: false, vertical: true)
-            }
-            Section {
-                Button("Chat Shortcuts") {
-                    isChatShortcutsSheetOpen = true
+        TabView {
+            Text("To be implemented")
+                .tabItem {
+                    Text("General")
                 }
-            } footer: {
-                Text("Save chats to app's menubar and easily access them")
-                    .font(.caption)
-            }
-            .sheet(isPresented: $isChatShortcutsSheetOpen) {
-                HStack {
-                    VStack {
-                        Text("Chat shortcuts")
-                            .font(.largeTitle)
-                        Text("Save chats to app's menubar for easy access from any place in Moc.")
-                            .font(.title3)
-                    }.frame(width: 100)
-                    VStack {
-                        Button {
-                            isChatShortcutsSheetOpen = false
-                        } label: {
-                            Image(systemName: "xmark")
-                        }
-                        .buttonStyle(.plain)
-                        .hTrailing()
-                        List {
-                            ForEach(chatShortcuts, id: \.self) { chatId in
-                                HStack {
-                                    CompactChatItemView(chatId: chatId)
-                                }
-                                .swipeActions(edge: .trailing) {
-                                    Button(role: .destructive) {
-                                        chatShortcuts.removeAll(where: { $0 == chatId })
-                                    } label: {
-                                        Label("Remove", systemImage: "trash")
-                                    }
+            HStack(spacing: 16) {
+                VStack {
+                    Text("Chat shortcuts")
+                        .font(.largeTitle)
+                    Text("Save chats to app's menubar for easy access from any place in Moc.")
+                    Spacer()
+                }
+                .frame(width: 300)
+                VStack {
+                    List {
+                        ForEach(chatShortcuts, id: \.self) { chatId in
+                            HStack {
+                                CompactChatItemView(chatId: chatId)
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button(role: .destructive) {
+                                    chatShortcuts.removeAll(where: { $0 == chatId })
+                                } label: {
+                                    Label("Remove", systemImage: "trash")
                                 }
                             }
                         }
-                        #if os(macOS)
-                        .listStyle(.bordered(alternatesRowBackgrounds: true))
-                        #endif
-                        Button("Add") {
-                            isNewChatShortcutSheetOpen = true
-                        }
-                        .sheet(isPresented: $isNewChatShortcutSheetOpen) {
-                            VStack(spacing: 8) {
-                                Button {
-                                    isNewChatShortcutSheetOpen = false
-                                } label: {
-                                    Image(systemName: "xmark")
-                                }
-                                .buttonStyle(.plain)
-                                .hTrailing()
-                                Text(
+                    }
+#if os(macOS)
+                    .listStyle(.bordered(alternatesRowBackgrounds: true))
+#endif
+                    Button("Add") {
+                        isNewChatShortcutSheetOpen = true
+                    }
+                    .sheet(isPresented: $isNewChatShortcutSheetOpen) {
+                        VStack(spacing: 8) {
+                            Button {
+                                isNewChatShortcutSheetOpen = false
+                            } label: {
+                                Image(systemName: "xmark")
+                            }
+                            .buttonStyle(.plain)
+                            .hTrailing()
+                            Text(
                             """
                             Chat picker is in development, will be done in Stage 3
                             Please insert the chat ID instead, you can find it \
                             in the chat inspector with "Show developer info" \
                             enabled
                             """)
-                                .lineLimit(nil)
-                                .multilineTextAlignment(.center)
-                                .fixedSize(horizontal: false, vertical: true)
-                                TextField("Chat ID", value: $chatId, formatter: NumberFormatter())
-                                    .onSubmit {
-                                        if chatId != 0 {
-                                            chatShortcuts.append(chatId)
-                                            isNewChatShortcutSheetOpen = false
-                                            self.chatId = 0
-                                        }
+                            .lineLimit(nil)
+                            .multilineTextAlignment(.center)
+                            .fixedSize(horizontal: false, vertical: true)
+                            TextField("Chat ID", value: $chatId, formatter: NumberFormatter())
+                                .onSubmit {
+                                    if chatId != 0 {
+                                        chatShortcuts.append(chatId)
+                                        isNewChatShortcutSheetOpen = false
+                                        self.chatId = 0
                                     }
-                                    .textFieldStyle(.roundedBorder)
-                            }
-                            .frame(maxWidth: 200, maxHeight: 300)
-                            .padding()
+                                }
+                                .textFieldStyle(.roundedBorder)
                         }
+                        .frame(maxWidth: 200, maxHeight: 300)
+                        .padding()
                     }
                 }
-                .frame(minWidth: 250, minHeight: 300)
-                .padding(8)
+            }
+            .frame(minWidth: 250, minHeight: 300)
+            .padding(8)
+            .tabItem {
+                Text("Chat shortcuts")
+            }
+            VStack {
+                Form {
+                    Section {
+                        Defaults.Toggle("Show developer info", key: .showDeveloperInfo)
+                    } footer: {
+                        Text("""
+                    When enabled, Moc will show additional data in the \
+                    chat inspector like the chat/user ID.
+                    """)
+                        .font(.caption)
+                        .fixedSize(horizontal: false, vertical: true)
+                    }
+                }
+                Spacer()
+            }
+            .tabItem {
+                Text("Advanced")
             }
         }
-        .padding(8)
+        .tabViewStyle(.automatic)
+        .padding()
     }
 }
 
