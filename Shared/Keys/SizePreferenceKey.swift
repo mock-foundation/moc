cf711bc46b5b980d72accbdf17f64ee9091f2e8a
Lev Poznyakov <levochkaaa@icloud.com>
2022-09-17 23:05:48 +0300

    users list in chat inspector

---
+++ Shared/Keys/SizePreferenceKey.swift
@@ -0,0 +1,12 @@
+// SizePreferenceKey.swift
+
+import Foundation
+import SwiftUI
+
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
+    }
+}
