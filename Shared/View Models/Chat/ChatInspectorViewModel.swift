26fd2fe8857815960831ab5a2c998e2a92029d06
Lev Poznyakov <62802017+levochkaa@users.noreply.github.com>
2022-09-20 02:32:24 +0300

    Members in Chat Inspector (#153)

--- Shared/View Models/Chat/ChatInspectorViewModel.swift
+++ Shared/View Models/Chat/ChatInspectorViewModel.swift
@@ -27,17 +27,24 @@ class ChatInspectorViewModel: ObservableObject {
             }
         }
     }
-    
+    private var tdApi: TdApi = .shared
+    private var members = [ChatMember]()
+    private var chat: Chat?
+
+    var loadedUsers = 0
+    var loadingUsers = false
+
     @Published var chatPhoto: File?
     @Published var chatTitle = ""
-    @Published var chatMemberCount: Int?
+    @Published var chatMemberCount: Int? 
+    @Published var chatMembers = [User]()
     @Published var selectedInspectorTab: ChatInspectorTab = .users
-    
+
     init(chatId: Int64) {
         self.chatId = chatId
 
         Task { try await updateInfo() }
-        
+
         service.updateSubject
             .receive(on: RunLoop.main)
             .sink { update in
@@ -55,12 +62,85 @@ class ChatInspectorViewModel: ObservableObject {
             }
             .store(in: &subscribers)
     }
-    
+
     func updateInfo() async throws {
-        let chat = try await service.getChat(with: chatId)
+        self.chat = try await service.getChat(with: chatId)
+        guard let chat else { return }
+
         DispatchQueue.main.async {
             self.chatPhoto = chat.photo?.big
             self.chatTitle = chat.title
+            self.chatMembers = []
         }
+
+        self.loadedUsers = 0
+        try await loadMembers(isInitial: true)
+    }
+
+    func loadMembers(isInitial: Bool = false) async throws {
+        guard let chat else { return }
+
+        if !loadingUsers {
+            self.loadingUsers = true
+        } else {
+            return
+        }
+
+        switch chat.type {
+            case .basicGroup(let basicGroup):
+                if isInitial {
+                    let info = try await tdApi.getBasicGroupFullInfo(basicGroupId: basicGroup.basicGroupId)
+                    self.members = info.members
+
+                    DispatchQueue.main.async {
+                        self.chatMemberCount = self.members.count
+                    }
+
+                    try await loadChatMembers(isInitial: isInitial)
+                } else {
+                    break
+                }
+            case .supergroup(let supergroup):
+                if isInitial {
+                    let info = try await tdApi.getSupergroupFullInfo(supergroupId: supergroup.supergroupId)
+                    DispatchQueue.main.async {
+                        self.chatMemberCount = info.memberCount
+                    }
+                }
+
+                if self.chatMemberCount == loadedUsers { break }
+
+                let supergroupMembers = try await tdApi.getSupergroupMembers(
+                    filter: nil,
+                    limit: 10,
+                    offset: loadedUsers,
+                    supergroupId: supergroup.supergroupId
+                )
+
+                self.members = supergroupMembers.members
+                loadedUsers += self.members.count
+
+                try await loadChatMembers(isInitial: isInitial)
+            default:
+                break
+        }
+    }
+
+    private func loadChatMembers(isInitial: Bool = false) async throws {
+        let mappedUsers = try await self.members.asyncCompactMap { member in
+            switch member.memberId {
+                case .user(let sender):
+                    let user = try await tdApi.getUser(userId: sender.userId)
+                    return user
+                default:
+                    return nil
+            }
+        }
+
+        DispatchQueue.main.async {
+            self.chatMembers += mappedUsers
+        }
+        
+        self.loadingUsers = false
     }
 }
--- Shared/Views/Chat/ChatInspector.swift
+++ Shared/Views/Chat/ChatInspector.swift
@@ -11,14 +11,15 @@ import Defaults
 
 struct ChatInspector: View {
     let chatId: Int64
+    @State private var headerHeight: Int = 0
     @StateObject private var viewModel: ChatInspectorViewModel
     @Default(.showDeveloperInfo) var showDeveloperInfo
-    
+
     init(id: Int64) {
         self.chatId = id
         self._viewModel = StateObject(wrappedValue: ChatInspectorViewModel(chatId: id))
     }
-    
+
     private func makeInspectorButton(action: @escaping () -> Void, imageName: String, text: String) -> some View {
         Button(action: action) {
             VStack(spacing: 8) {
@@ -31,24 +32,37 @@ struct ChatInspector: View {
         .frame(width: 48, height: 48)
         .buttonStyle(.borderless)
     }
-    
-    private func makeUserRow(name: String, status: UserStatus, photo: Image? = nil) -> some View {
+
+    @ViewBuilder private func makeUserRow(for user: User) -> some View {
+        let placeholder = ProfilePlaceholderView(userId: user.id,
+                                                 firstName: user.firstName,
+                                                 lastName: user.lastName,
+                                                 style: .miniature)
         HStack {
-            if photo != nil {
-                photo!
-                    .resizable()
-                    .frame(width: 32, height: 32)
-                    .clipShape(Circle())
-                    .padding(8)
+            Group {
+                if let photo = user.profilePhoto {
+                    AsyncTdImage(id: photo.small.id) { image in
+                        image
+                            .resizable()
+                    } placeholder: {
+                        placeholder
+                    }
+                } else {
+                    placeholder
+                }
             }
+            .frame(width: 32, height: 32)
+            .clipShape(Circle())
+            .padding(8)
+
             VStack(alignment: .leading) {
-                Text(name)
+                Text("\(user.firstName) \(user.lastName)")
                 Text("User status")
             }
             Spacer()
         }
     }
-    
+
     private func makePlaceholder(_ style: PlaceholderStyle) -> some View {
         ProfilePlaceholderView(
             userId: chatId,
@@ -57,110 +71,154 @@ struct ChatInspector: View {
             style: style
         )
     }
-    
+
+    @ViewBuilder private var headerView: some View {
+        if let photo = viewModel.chatPhoto {
+            AsyncTdImage(id: photo.id) { image in
+                image
+                    .resizable()
+                    .interpolation(.medium)
+                    .antialiased(true)
+            } placeholder: {
+                makePlaceholder(.medium)
+            }
+            .frame(width: 86, height: 86)
+            .clipShape(Circle())
+        } else {
+            makePlaceholder(.medium)
+                .frame(width: 86, height: 86)
+                .clipShape(Circle())
+        }
+        VStack {
+            Text(viewModel.chatTitle)
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.horizontal)
+                .frame(minWidth: 0, idealWidth: nil)
+                .multilineTextAlignment(.center)
+            if showDeveloperInfo {
+                Text("ID: \(String(chatId).trimmingCharacters(in: .whitespaces))")
+                    .textSelection(.enabled)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        Text("\(viewModel.chatMemberCount ?? 0) members")
+            .fontWeight(.medium)
+    }
+
+    @ViewBuilder private var quickActionsView: some View {
+        HStack(spacing: 24) {
+            makeInspectorButton(
+                action: {},
+                imageName: "person.crop.circle.badge.plus",
+                text: "Add"
+            )
+            makeInspectorButton(
+                action: {},
+                imageName: "bell.slash",
+                text: "Mute"
+            )
+            makeInspectorButton(
+                action: {},
+                imageName: "arrow.turn.up.right",
+                text: "Leave"
+            )
+            .tint(.red)
+        }
+        .padding(.vertical)
+        .frame(minWidth: 0, idealWidth: nil)
+    }
+
+    @ViewBuilder private var sectionsView: some View {
+        Section {
+            switch viewModel.selectedInspectorTab {
+                case .users:
+                    ZStack {
+                        LazyVStack {
+                            ForEach(viewModel.chatMembers, id: \.id) { member in
+                                makeUserRow(for: member)
+                                    .padding(.horizontal, 8)
+                                    .frame(minWidth: 0, idealWidth: nil)
+                            }
+                        }
+                        GeometryReader { proxy in
+                            Color.clear.preference(
+                                key: ScrollOffsetPreferenceKey.self,
+                                value: Int(proxy.frame(in: .named("scrollUsers")).maxY))
+                        }
+                    }
+                case .media:
+                    Text("Media")
+                case .links:
+                    Text("Links")
+                case .files:
+                    Text("Files")
+                case .voice:
+                    Text("Voice")
+            }
+        } header: {
+            Picker(selection: $viewModel.selectedInspectorTab) {
+                Text("Users").tag(ChatInspectorTab.users)
+                Text("Media").tag(ChatInspectorTab.media)
+                Text("Links").tag(ChatInspectorTab.links)
+                Text("Files").tag(ChatInspectorTab.files)
+                Text("Voice").tag(ChatInspectorTab.voice)
+            } label: {
+                EmptyView()
+            }
+            .pickerStyle(.segmented)
+            .controlSize(.large)
+            .padding()
+            .frame(minWidth: 0, idealWidth: nil)
+            .background(.ultraThinMaterial, in: CornerRectangle(tl: 0, tr: 0, bl: 8, br: 8))
+        }
+    }
+
     var body: some View {
         ScrollView {
             LazyVStack(spacing: 16, pinnedViews: .sectionHeaders) {
-                // Header
-                if let photo = viewModel.chatPhoto {
-                    AsyncTdImage(id: photo.id) { image in
-                        image
-                            .resizable()
-                            .interpolation(.medium)
-                            .antialiased(true)
-                    } placeholder: {
-                        makePlaceholder(.medium)
-                    }
-                    .frame(width: 86, height: 86)
-                    .clipShape(Circle())
-                } else {
-                    makePlaceholder(.medium)
-                        .frame(width: 86, height: 86)
-                        .clipShape(Circle())
+                Group {
+                    headerView
+
+                    quickActionsView
                 }
-                VStack {
-                    Text(viewModel.chatTitle)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .padding(.horizontal)
-                        .frame(minWidth: 0, idealWidth: nil)
-                        .multilineTextAlignment(.center)
-                    if showDeveloperInfo {
-                        Text("ID: \(String(chatId).trimmingCharacters(in: .whitespaces))")
-                            .textSelection(.enabled)
-                            .foregroundStyle(.secondary)
+                .background {
+                    GeometryReader { geometry in
+                        Color.clear.preference(
+                            key: SizePreferenceKey.self,
+                            value: geometry.size
+                        )
                     }
                 }
-                Text("\(viewModel.chatMemberCount ?? 0) members")
-                    .fontWeight(.medium)
-                
-                // Quick actions
-                HStack(spacing: 24) {
-                    makeInspectorButton(
-                        action: {},
-                        imageName: "person.crop.circle.badge.plus",
-                        text: "Add"
-                    )
-                    makeInspectorButton(
-                        action: {},
-                        imageName: "bell.slash",
-                        text: "Mute"
-                    )
-                    makeInspectorButton(
-                        action: {},
-                        imageName: "arrow.turn.up.right",
-                        text: "Leave"
-                    )
-                    .tint(.red)
-                }
-                .padding(.vertical)
-                .frame(minWidth: 0, idealWidth: nil)
-                
-                // More info
-                Section {
-                    ScrollView {
-                        switch viewModel.selectedInspectorTab {
-                            case .users:
-                                ForEach(0 ..< 10) { index in
-                                    makeUserRow(
-                                        name: "User \(index)",
-                                        status: .recently,
-                                        photo: Image("MockChatPhoto")
-                                    )
-                                    .padding(.horizontal, 8)
-                                    .frame(minWidth: 0, idealWidth: nil)
-                                }
-                            case .media:
-                                Text("Media")
-                            case .links:
-                                Text("Links")
-                            case .files:
-                                Text("Files")
-                            case .voice:
-                                Text("Voice")
-                        }
+                .onPreferenceChange(SizePreferenceKey.self) { size in
+                    if self.headerHeight != 0 {
+                        self.headerHeight = Int(size.height)
                     }
-                } header: {
-                    Picker(selection: $viewModel.selectedInspectorTab) {
-                        Text("Users").tag(ChatInspectorTab.users)
-                        Text("Media").tag(ChatInspectorTab.media)
-                        Text("Links").tag(ChatInspectorTab.links)
-                        Text("Files").tag(ChatInspectorTab.files)
-                        Text("Voice").tag(ChatInspectorTab.voice)
-                    } label: {
-                        EmptyView()
-                    }
-                    .pickerStyle(.segmented)
-                    .controlSize(.large)
-                    .padding()
-                    .frame(minWidth: 0, idealWidth: nil)
-                    .background(.ultraThinMaterial, in: CornerRectangle(tl: 0, tr: 0, bl: 8, br: 8))
                 }
+
+                sectionsView
             }
             .padding(.top)
         }
+        .coordinateSpace(name: "scrollUsers")
+        .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+            let newValue = value - headerHeight
+            let range = (850...950)
+            if !viewModel.loadingUsers && range.contains(newValue) {
+                Task {
+                    do {
+                        try await viewModel.loadMembers()
+                    } catch {
+                        viewModel.loadingUsers = false
+                    }
+                }
+            }
+        }
         .onChange(of: chatId) { newValue in
             viewModel.chatId = newValue
+            Task {
+                try await viewModel.updateInfo()
+            }
         }
     }
 }
