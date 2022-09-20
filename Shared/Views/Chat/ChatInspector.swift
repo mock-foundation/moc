3a19807048c4975ba3b9180606aef5ca821836a9
Lev Poznyakov <levochkaaa@icloud.com>
2022-09-20 18:37:16 +0300

    replacement of a scroll with list

--- Shared/Views/Chat/ChatInspector.swift
+++ Shared/Views/Chat/ChatInspector.swift
@@ -57,110 +57,161 @@ struct ChatInspector: View {
             style: style
         )
     }
-    
-    var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 16, pinnedViews: .sectionHeaders) {
-                // Header
-                if let photo = viewModel.chatPhoto {
-                    AsyncTdImage(id: photo.id) { image in
-                        image
-                            .resizable()
-                            .interpolation(.medium)
-                            .antialiased(true)
-                    } placeholder: {
-                        makePlaceholder(.medium)
-                    }
-                    .frame(width: 86, height: 86)
-                    .clipShape(Circle())
-                } else {
-                    makePlaceholder(.medium)
-                        .frame(width: 86, height: 86)
-                        .clipShape(Circle())
-                }
-                VStack {
-                    Text(viewModel.chatTitle)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .padding(.horizontal)
-                        .frame(minWidth: 0, idealWidth: nil)
-                        .multilineTextAlignment(.center)
-                    if showDeveloperInfo {
-                        Text("ID: \(String(chatId).trimmingCharacters(in: .whitespaces))")
-                            .textSelection(.enabled)
-                            .foregroundStyle(.secondary)
-                    }
-                }
-                Text("\(viewModel.chatMemberCount ?? 0) members")
-                    .fontWeight(.medium)
-                
-                // Quick actions
-                HStack(spacing: 24) {
-                    makeInspectorButton(
-                        action: {},
-                        imageName: "person.crop.circle.badge.plus",
-                        text: "Add"
-                    )
-                    makeInspectorButton(
-                        action: {},
-                        imageName: "bell.slash",
-                        text: "Mute"
-                    )
-                    makeInspectorButton(
-                        action: {},
-                        imageName: "arrow.turn.up.right",
-                        text: "Leave"
-                    )
-                    .tint(.red)
-                }
-                .padding(.vertical)
+
+    @ViewBuilder private var headerView: some View {
+        if let photo = viewModel.chatPhoto {
+            AsyncTdImage(id: photo.id) { image in
+                image
+                    .resizable()
+                    .interpolation(.medium)
+                    .antialiased(true)
+            } placeholder: {
+                makePlaceholder(.medium)
+            }
+            .frame(width: 86, height: 86)
+            .clipShape(Circle())
+        } else {
+            makePlaceholder(.medium)
+                .frame(width: 86, height: 86)
+                .clipShape(Circle())
+        }
+        VStack {
+            Text(viewModel.chatTitle)
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.horizontal)
                 .frame(minWidth: 0, idealWidth: nil)
-                
-                // More info
-                Section {
-                    ScrollView {
-                        switch viewModel.selectedInspectorTab {
-                            case .users:
-                                ForEach(0 ..< 10) { index in
-                                    makeUserRow(
-                                        name: "User \(index)",
-                                        status: .recently,
-                                        photo: Image("MockChatPhoto")
-                                    )
+                .multilineTextAlignment(.center)
+            if showDeveloperInfo {
+                Text("ID: \(String(chatId).trimmingCharacters(in: .whitespaces))")
+                    .textSelection(.enabled)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        Text("\(viewModel.chatMemberCount ?? 0) members")
+            .fontWeight(.medium)
+    }
+
+    @ViewBuilder private var quickActionsView: some View {
+        HStack(spacing: 24) {
+            makeInspectorButton(
+                action: {},
+                imageName: "person.crop.circle.badge.plus",
+                text: "Add"
+            )
+            makeInspectorButton(
+                action: {},
+                imageName: "bell.slash",
+                text: "Mute"
+            )
+            makeInspectorButton(
+                action: {},
+                imageName: "arrow.turn.up.right",
+                text: "Leave"
+            )
+            .tint(.red)
+        }
+        .padding(.vertical)
+        .frame(minWidth: 0, idealWidth: nil)
+    }
+
+    @ViewBuilder private var sectionsView: some View {
+        Section {
+            switch viewModel.selectedInspectorTab {
+                case .users:
+                    ZStack {
+                        LazyVStack {
+                            ForEach(viewModel.chatMembers, id: \.id) { member in
+                                makeUserRow(for: member)
                                     .padding(.horizontal, 8)
                                     .frame(minWidth: 0, idealWidth: nil)
-                                }
-                            case .media:
-                                Text("Media")
-                            case .links:
-                                Text("Links")
-                            case .files:
-                                Text("Files")
-                            case .voice:
-                                Text("Voice")
+                            }
+                        }
+                        GeometryReader { proxy in
+                            Color.clear.preference(
+                                key: ScrollOffsetPreferenceKey.self,
+                                value: Int(proxy.frame(in: .named("scrollUsers")).maxY))
                         }
                     }
-                } header: {
-                    Picker(selection: $viewModel.selectedInspectorTab) {
-                        Text("Users").tag(ChatInspectorTab.users)
-                        Text("Media").tag(ChatInspectorTab.media)
-                        Text("Links").tag(ChatInspectorTab.links)
-                        Text("Files").tag(ChatInspectorTab.files)
-                        Text("Voice").tag(ChatInspectorTab.voice)
-                    } label: {
-                        EmptyView()
+                case .media:
+                    Text("Media")
+                case .links:
+                    Text("Links")
+                case .files:
+                    Text("Files")
+                case .voice:
+                    Text("Voice")
+            }
+        } header: {
+            Picker(selection: $viewModel.selectedInspectorTab) {
+                Text("Users").tag(ChatInspectorTab.users)
+                Text("Media").tag(ChatInspectorTab.media)
+                Text("Links").tag(ChatInspectorTab.links)
+                Text("Files").tag(ChatInspectorTab.files)
+                Text("Voice").tag(ChatInspectorTab.voice)
+            } label: {
+                EmptyView()
+            }
+            .pickerStyle(.segmented)
+            .controlSize(.large)
+            .frame(minWidth: 0, idealWidth: nil)
+        }
+    }
+
+    var body: some View {
+        ScrollViewReader { proxy in
+            List {
+                VStack(spacing: 16) {
+                    VStack {
+                        headerView
+                            .id("header")
+
+                        quickActionsView
+                            .id("quickActions")
                     }
-                    .pickerStyle(.segmented)
-                    .controlSize(.large)
-                    .padding()
-                    .frame(minWidth: 0, idealWidth: nil)
-                    .background(.ultraThinMaterial, in: CornerRectangle(tl: 0, tr: 0, bl: 8, br: 8))
+                    .background {
+                        GeometryReader { geometry in
+                            Color.clear.preference(
+                                key: SizePreferenceKey.self,
+                                value: geometry.size
+                            )
+                        }
+                    }
+                    .onPreferenceChange(SizePreferenceKey.self) { size in
+                        if self.headerHeight != 0 {
+                            self.headerHeight = Int(size.height)
+                        }
+                    }
+
+                    sectionsView
+                        .id("sections")
+                }
+                .padding(.top)
+            }
+            .coordinateSpace(name: "scrollUsers")
+            .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+                let newValue = value - headerHeight
+                let range = (800...950)
+                if !viewModel.loadingUsers && range.contains(newValue) {
+                    Task {
+                        do {
+                            try await viewModel.loadMembers()
+                        } catch {
+                            viewModel.loadingUsers = false
+                        }
+                    }
                 }
             }
-            .padding(.top)
-        }
-        .onChange(of: chatId) { newValue in
-            viewModel.chatId = newValue
+            .onChange(of: chatId) { newValue in
+                viewModel.chatId = newValue
+                Task {
+                    try await viewModel.updateInfo()
+                }
+                proxy.scrollTo("header", anchor: .top)
+            }
+            .onChange(of: viewModel.selectedInspectorTab) { _ in
+                proxy.scrollTo("sections", anchor: .top)
+            }
         }
     }
 }
