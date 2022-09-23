037b2e412989294a08a4b0b151b6903333c0fc0e
GGorAA <ggoraa1029@gmail.com>
2022-09-23 21:59:53 +0200

    other: removed superfluous swiftlint:disable comment

--- Shared/Views/Chat/Message/MessageView+Album.swift
+++ Shared/Views/Chat/Message/MessageView+Album.swift
@@ -10,17 +10,11 @@ import TDLibKit
 import SkeletonUI
 
 extension MessageView {
-    // swiftlint:disable function_body_length cyclomatic_complexity
     /// Just makes the album. Used in ``makeMessageVideo(from:)``
     /// and ``makeMessagePhoto(from:)`` to not repeate the same code twice
     func makeAlbum() -> some View {
         makeMessage {
             VStack(spacing: 0) {
-                // NOTE: all of this code is only for macOS Monterey
-                // and iPadOS 15. It's a subject for change when I will
-                // get familiar with new Layout API in macOS Ventura
-                // and iPadOS 16, so I can build a better system for
-                // organizing media in an album
                 if message.first(where: { $0.content.isGraphic }) != nil {
                     if #available(macOS 13, iOS 16, *) {
                         MediaAlbum {
