236d8611dc9e58be2bc70df9fcd4ed530434d430
GGorAA <ggoraa1029@gmail.com>
2022-10-12 02:48:12 +0200

    feat: added a keyboard shortcut for archive chat list

--- MenuBar/Sources/MenuBar/ChatCommand.swift
+++ MenuBar/Sources/MenuBar/ChatCommand.swift
@@ -11,14 +11,22 @@ import Utilities
 public struct ChatCommand: Commands {
     public init() { }
     
+    @State private var archiveChatList = false
+    
     public var body: some Commands {
-        CommandMenu("Chat") {
+        CommandMenu("Chats") {
+            Toggle("Open archive chat list", isOn: $archiveChatList.didSet {
+                sendUpdate(.toggle(.toggleArchive, $0))
+            })
+            .keyboardShortcut("A", modifiers: [.command, .option])
+            Divider()
             Button("Toggle chat inspector") {
                 sendUpdate(.trigger(.toggleChatInspector))
-            }.keyboardShortcut("I")
+            }.keyboardShortcut("I", modifiers: .command)
             Button("Toggle chat info") {
                 sendUpdate(.trigger(.toggleChatInfo))
             }.keyboardShortcut("I", modifiers: [.command, .shift])
+            
         }
     }
 }
