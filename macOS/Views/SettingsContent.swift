f3340914c84fcf9888abef334713d93d1a4f616f
GGorAA <ggoraa1029@gmail.com>
2022-11-01 03:33:05 +0100

    feat: localized macOS settings tab names (except of general and account)

--- macOS/Views/SettingsContent.swift
+++ macOS/Views/SettingsContent.swift
@@ -17,47 +17,42 @@ struct SettingsContent: View {
                 }
             NotificationsAndSoundsPrefView()
                 .tabItem {
-                    Label("Notifications and Sounds", systemImage: "bell")
+                    Label(l10n: "Settings.NotificationsAndSounds", systemImage: "bell")
                 }
             PrivacyPrefView()
                 .tabItem {
-                    Label("Privacy", systemImage: "lock")
+                    Label(l10n: "Settings.PrivacySettings", systemImage: "lock")
                 }
             DataAndStoragePrefView()
                 .tabItem {
-                    Label("Data and Storage", systemImage: "externaldrive")
+                    Label(l10n: "Settings.ChatSettings", systemImage: "externaldrive")
                 }
             DevicesPrefView()
                 .tabItem {
-                    Label("Devices", systemImage: "laptopcomputer.and.iphone")
+                    Label(l10n: "Settings.Devices", systemImage: "laptopcomputer.and.iphone")
                 }
             AppearancePrefView()
                 .tabItem {
-                    Label("Appearance", systemImage: "paintbrush")
+                    Label(l10n: "Settings.Appearance", systemImage: "paintbrush")
                 }
             LanguagePrefView()
                 .tabItem {
-                    Label {
-                        L10nText("Settings.AppLanguage")
-                    } icon: {
-                        Image(systemName: "globe")
-                    }
-                    Label("Language", systemImage: "globe")
+                    Label(l10n: "Settings.AppLanguage", systemImage: "globe")
                 }
             StickersPrefView()
                 .tabItem {
-                    Label("Stickers", systemImage: "rectangle.3.group.bubble.left")
+                    Label(l10n: "ChatSettings.StickersAndReactions", systemImage: "rectangle.3.group.bubble.left")
                 }
             FoldersPrefView()
                 .tabItem {
-                    Label("Folders", systemImage: "folder")
+                    Label(l10n: "Settings.ChatFolders", systemImage: "folder")
                 }
             AccountsPrefView()
                 .tabItem {
                     Label("Accounts", systemImage: "person.circle")
                 }
         }
-        .frame(width: 800)
+        .frame(width: 900)
     }
 }
 
